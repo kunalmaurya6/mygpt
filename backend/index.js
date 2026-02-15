77992b6c2f1945fbf2110f66456329bff96057e5
kunalmaurya6 <109097899+kunalmaurya6@users.noreply.github.com>
2026-02-15 15:48:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 //app.use(cors());
 app.use(cors({
-    origin: "https://mygpt-5622.vercel.app/",
+    origin: "https://mygpt-5622.vercel.app",
     credentials: true
 }));
 app.use(cookieParser());
